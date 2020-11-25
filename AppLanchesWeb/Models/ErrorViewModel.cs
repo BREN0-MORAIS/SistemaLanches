72f9ffcaa48a15856d6a7d27784ce137dcf00377
BRENO-MORAIS <brenofranciscomorais@gmail.com>
2020-11-24 21:59:46 -0400

    foi criado o projeto

---
+++ AppLanchesWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppLanchesWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
