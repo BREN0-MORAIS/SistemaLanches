6660f76dcc77d243046430ce3bd40cc8a9cfe05f
BRENO-MORAIS <brenofranciscomorais@gmail.com>
2020-11-30 06:01:14 -0400

    Ajustado as Configuração de Conexão e criado o Contexto.

--- AppLanchesWeb/Startup.cs
+++ AppLanchesWeb/Startup.cs
@@ -1,5 +1,7 @@
+using AppLanchesWeb.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,6 +24,7 @@ namespace AppLanchesWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddControllersWithViews();
         }
 
